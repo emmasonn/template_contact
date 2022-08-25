74145e9d82d68befd4a62b557b7c7693f30cda81
emmasonn <words.cs1@gmail.com>
2022-08-25 05:35:05 +0100

    modified

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_app/features/contact/app/provider/contact_manager.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt sl = GetIt.instance;
 
 Future<void> init() async {
+  //*contact manager
+  sl.registerFactory(() => ContactManager());
+
 
 }
